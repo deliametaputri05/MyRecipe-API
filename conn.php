7b9d52a2cbd2391357d8461fd6f19ff78cd17152
deliametaputri05 <deliametaputri05@gmail.com>
2021-04-22 15:41:21 +0800

    Koneksi dan Login User

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = new mysqli("localhost", "root", "", "db_myrecipe");
+
+// if ($connect) {
+//     echo "Sukses";
+// } else {
+//     echo "Connection Failed";
+//     exit();
+// }
